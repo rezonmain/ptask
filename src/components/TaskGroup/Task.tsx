c0b4a0d7a01ab4df79a2e6f645e174d0edb655f0
rezonmain <adiazgomez101@gmail.com>
2023-03-20 21:56:26 -0700

    feat: added y draggable

--- src/components/TaskGroup/Task.tsx
+++ src/components/TaskGroup/Task.tsx
@@ -1,12 +1,34 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { useSpring, animated } from "@react-spring/web";
 import ITask from "../../classes/Task";
+import { useDrag } from "@use-gesture/react";
 
-const Task = ({ task }: { task: ITask }) => {
+interface Props extends ComponentPropsWithoutRef<"div"> {
+  task: ITask;
+}
+
+const Task = forwardRef<HTMLDivElement, Props>(({ task, ...props }, ref) => {
+  const [{ y }, api] = useSpring(() => ({ y: 0 }));
+
+  const bind = useDrag(({ down, movement: [, my] }) => {
+    api.start({ y: down ? my : 0, immediate: down });
+  });
+
   return (
-    <details className="format">
-      <summary>{task.summary}</summary>
-      {task.details}
-    </details>
+    <animated.div
+      ref={ref}
+      {...props}
+      {...bind()}
+      style={{ y }}
+      className="touch-none"
+    >
+      <details className="format">
+        <summary>{task.summary}</summary>
+        {task.details}
+      </details>
+    </animated.div>
   );
-};
+});
 
+Task.displayName = "Task";
 export default Task;
